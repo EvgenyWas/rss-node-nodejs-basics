bed8ab352ce46e92825f6cdecd4852acd0286611
EvgenyWas <evgeniywas@gmail.com>
2024-10-02 21:56:28 +0100

    feat: implement nodejs basics cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,12 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argv = process.argv.slice(2);
+  const args = [];
+  for (let i = 0; i < argv.length; i += 2) {
+    const key = argv[i].replace("--", "");
+    const value = argv[i + 1];
+    args.push(`${key} is ${value}`);
+  }
+  console.log(args.join(", "));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = [];
+  for (const key in process.env) {
+    if (key.startsWith("RSS_")) {
+      variables.push(`${key}=${process.env[key]}`);
+    }
+  }
+  console.log(variables.join("; "));
 };
 
 parseEnv();
