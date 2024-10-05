69374f0b819d84e3276c85a73449e173fcc5304a
EvgenyWas <evgeniywas@gmail.com>
2024-10-05 14:41:03 +0100

    feat: implement nodejs basics zlib tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,22 @@
+import stream from "node:stream/promises";
+import fs from "node:fs";
+import zlib from "node:zlib";
+import path from "node:path";
+
 const compress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = path.resolve(__dirname, "files", "fileToCompress.txt");
+  const archivePath = path.resolve(__dirname, "files", "archive.gz");
+  try {
+    await fs.promises.access(filePath);
+    await stream.pipeline(
+      fs.createReadStream(filePath),
+      zlib.createGzip(),
+      fs.createWriteStream(archivePath)
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,23 @@
+import stream from "node:stream/promises";
+import fs from "node:fs";
+import zlib from "node:zlib";
+import path from "node:path";
+
 const decompress = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const archivePath = path.resolve(__dirname, "files", "archive.gz");
+  const filePath = path.resolve(__dirname, "files", "fileToCompress.txt");
+
+  try {
+    await fs.promises.access(archivePath);
+    await stream.pipeline(
+      fs.createReadStream(archivePath),
+      zlib.createGunzip(),
+      fs.createWriteStream(filePath)
+    );
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await decompress();
