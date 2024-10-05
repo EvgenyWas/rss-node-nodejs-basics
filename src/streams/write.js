a449777cf442a1d8d1836958006465687a8a1810
EvgenyWas <evgeniywas@gmail.com>
2024-10-05 18:20:54 +0100

    feat: implement nodejs basics streams tasks

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import fs from "node:fs";
+import path from "node:path";
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = import.meta.dirname;
+  const filePath = path.resolve(__dirname, "files", "fileToWrite.txt");
+  const stream = fs.createWriteStream(filePath);
+
+  process.stdin.pipe(stream);
 };
 
 await write();
